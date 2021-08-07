5f4cba377d57ddc2268d949e127e3d3ecce95d0b
paesser <33548055+Paesser@users.noreply.github.com>
2021-08-07 18:57:41 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCdRZ3GnDdbbkF6LZKxVFVxRA9IqN77_7I",
+    authDomain: "emaily-ba943.firebaseapp.com",
+    projectId: "emaily-ba943",
+    storageBucket: "emaily-ba943.appspot.com",
+    messagingSenderId: "496622593966",
+    appId: "1:496622593966:web:fe268a2aa6b2f30a29ca3f",
+    measurementId: "G-8W9WZFKEQS"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
---
+++ script.js
@@ -0,0 +1,89 @@
+//registration script//
+
+
+function signup(){
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('pass').value;
+    
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in 
+    var user = userCredential.user;
+    // ...
+       document.getElementById("reg").style.display="none";
+		document.getElementById("logout").style.display="none";
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ..
+         console.log("error")
+  });
+    
+}
+
+//login script//
+
+function login(){
+	 var email = document.getElementById('email').value;
+    var password = document.getElementById('pass').value;
+	
+	firebase.auth().signInWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+		document.getElementById("form").style.display="none";
+		document.getElementById("logout").style.display="block";
+		document.getElementById("pTag").innerHTML=email;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+		
+		console.log("error")
+  });
+
+	
+}
+
+
+//logout//
+
+function logout(){
+	
+	firebase.auth().signOut().then(() => {
+		
+		document.getElementById("form").style.display="block";
+		document.getElementById("reg").style.display="block";
+		document.getElementById("logout").style.display="none";
+		document.getElementById("pTag").innerHTML="";
+  // Sign-out successful.
+}).catch((error) => {
+  // An error happened.
+		
+		console.log("error")
+});
+
+}
+
+function persist(){
+	
+	firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+  .then(() => {
+    // Existing and future Auth states are now persisted in the current
+    // session only. Closing the window would clear any existing state even
+    // if a user forgets to sign out.
+    // ...
+    // New sign-in will be persisted with session persistence.
+    return firebase.auth().signInWithEmailAndPassword(email, password);
+  })
+  .catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+		
+		console.log("could not persist")
+  });
+	
+}
